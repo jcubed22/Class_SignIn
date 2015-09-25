4815c211452f7c95e34636a3bc08b2833fb0e516
Jordan <jordanjohan@gmail.com>
2015-09-25 12:08:56 -0700

    Add hardcoded students.

--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,6 +1,6 @@
 classSignIn.factory('StudentsFactory', function StudentsFactory() {
   var factory = {};
-  factory.students = [];
+  factory.students = [{ name: "Johnny Student", isHere: false }, { name: "Jenny Studious", isHere: false }, { name: "That One Kid", isHere: false}, { name: "Wait, who?", isHere: false }, { name: "Polly Punctual", isHere: false }];
   factory.addStudent = function() {
     factory.students.push({ name: factory.studentName, isHere: false });
     factory.studentName = null;
